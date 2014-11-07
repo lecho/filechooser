29233f75b7fe07b8c0b0a5586a88034180ed5d78
Lecho <leszekwach@gmail.com>
2014-11-07 22:25:33 +0100

    Few additional javadocs

--- filechooser-library/src/lecho/lib/filechooser/FilechooserActivity.java
+++ filechooser-library/src/lecho/lib/filechooser/FilechooserActivity.java
@@ -4,6 +4,21 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.LocalBroadcastManager;
 
+/**
+ * Example usage:<br/><br/>
+ * 
+ * Intent i = new Intent(getActivity(), FilechooserActivity.class);<br/>
+ * i.putExtra(FilechooserActivity.BUNDLE_ITEM_TYPE, ItemType.FILE);<br/>
+ * i.putExtra(FilechooserActivity.BUNDLE_SELECTION_MODE, SelectionMode.SINGLE_ITEM);<br/>
+ * startActivityForResult(i, 1);<br/><br/>
+ * 
+ * 
+ * public void onActivityResult(int requestCode, int resultCode, Intent data) {<br/>
+ * if (resultCode == Activity.RESULT_OK) {<br/>
+ * ArrayList<String> paths = data.getStringArrayListExtra(FilechooserActivity.BUNDLE_SELECTED_PATHS);<br/>
+ * }<br/>
+ * }<br/>
+ */
 public class FilechooserActivity extends FragmentActivity {
 	public static final String BUNDLE_SELECTED_PATHS = "lecho.lib.filechooser:selected-paths";
 	public static final String BUNDLE_ITEM_TYPE = "lecho.lib.filechoser:item-type";
